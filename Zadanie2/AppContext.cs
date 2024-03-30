db57e3a9fdbe37e47a0c6b0a360ee6f76cfdaa4f
banika13 <44238236+banika13@users.noreply.github.com>
2024-03-30 13:52:52 +0500

    Add files via upload

---
+++ Zadanie2/AppContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Zadanie2
+{
+    internal class AppContext : DbContext
+    {
+        public DbSet<FIO> fios { get; set; } = null!;
+
+        public AppContext() 
+        { 
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server=localhost;user=root;password=govno;database=sys;",
+                new MySqlServerVersion(new Version(8, 0, 34)));
+        }
+    }
+}
---
+++ Zadanie2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Zadanie2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (AppContext  db = new AppContext())
+            {
+                var fios = db.fios.ToList();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Список людей");
+                Console.ResetColor();
+                foreach (var f in fios)
+                {
+                    Console.WriteLine($"{f.Id} {f.FirstName} {f.LastName} {f.Age}");
+                }
+            }
+        }
+    }
+}
